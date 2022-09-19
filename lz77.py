d9375b29a3cddfc54e863d9f7c0d88974ffc8371
ilandovprais <ilandovprais@gmail.com>
2022-09-19 22:37:00 +0300

    more efficient (~2 compression rate fairly fast)

--- lz77.py
+++ lz77.py
@@ -1,76 +1,122 @@
 from copyreg import pickle
+from re import T
 from typing import List, Tuple
 import numpy as np
 from functools import lru_cache
 import time
-import pickle
+from tqdm import tqdm
+from bitarray import bitarray
 
-def compress(text: str) -> bytes:
-    return str(generate_compressed_tuples(text)).encode()
+MAX_WINDOW_SIZE = 4000 # 12 bits 
+MAX_FORWARD_SIZE = 15 # 4 bits
 
-def decompress(compressed_text: bytes) -> str:
-    tuples = eval(compressed_text.decode())
-    return generate_text_from_tuples_recursively(tuples, "")
+class LZ77:
 
+    def __init__(self, window_size = 500, forward_size = 15) -> None:
+        assert window_size <= MAX_WINDOW_SIZE
+        assert forward_size <= MAX_FORWARD_SIZE
+        self.window_size = window_size
+        self.forward_size = forward_size
 
-def generate_text_from_tuples_recursively(tuples: List[Tuple], current_text: str):
-    current_text = ""
-    current_idx = 0
-    while current_idx < len(tuples):
-        offset, length, letter = tuples[current_idx]
-        current_text = current_text + letter
-        if length > offset + 1:
-            tuples[current_idx] = offset, length - offset, letter
-            current_text += current_text[-offset:-1] 
-        else:
-            current_idx += 1
-        current_text += current_text[-offset:-offset+length-1]
-    return current_text
+    def compress(self, file_path: str, compressed_file_path: str) -> bytes:
+        data = open(file_path, "rb").read(1000000)
 
+        output_buffer = bitarray(endian='big')
 
-def generate_compressed_tuples(text: str) -> Tuple[int, int, str]:
-    locs = []
-    i = 0
-    while i < len(text):
-        from_index, length = longest_match(text, i)
-        locs.append((i - from_index ,length, text[i]))
-        i += length
-    return locs
+        i = 0
+        with tqdm(total=len(data)) as p:
+            while i < len(data):
+                from_index, length = self._longest_match(data, i)
+
+                offset = i - from_index
+                letter = data[i]
+
+                # print(f"({offset}, {length}, {chr(letter)})")
+
+                if offset > 0:
+                    output_buffer.append(True)
+                    output_buffer.frombytes(bytes([offset >> 4]))
+                    output_buffer.frombytes(bytes([((offset & 0xf) << 4) | length]))
+                else:
+                    output_buffer.append(False)
+                    output_buffer.frombytes(bytes([letter]))
+
+                i += length
+                p.update(length)
+            
+        output_buffer.fill()
+
+        with open(compressed_file_path, "wb") as out:
+            out.write(output_buffer.tobytes())
+        
+        return output_buffer.tobytes()
             
 
-def longest_match(text: str, current_index: int) -> Tuple[int, int]:
-    
-    lens = np.array([longest_match_from_index(text, i, current_index) for i in range(current_index)])
-    from_index = np.argmax(lens) if len(lens) > 0 else None
-    length = np.max(lens) if len(lens) > 0 else 0
+    def decompress(self, compressed_file_path: str, decompressed_file_path: str) -> bytes:
 
-    if length == 0:
-        return current_index, 1
+        data = bitarray(endian='big')
+        data.frombytes(open(compressed_file_path, "rb").read())
+        output_buffer = []
 
-    return from_index, length
-    
+        with tqdm(total=len(data)) as p:
+            while len(data) > 0:
+                is_pointer = data.pop(0)
+
+                if is_pointer:
+                    byte1 = ord(data[0:8].tobytes())
+                    byte2 = ord(data[8:16].tobytes())
+
+                    del data[0:16]
+                    offset = (byte1 << 4) | (byte2 >> 4)
+                    length = (byte2 & 0xf)
+
+                    for i in range(length):
+                        output_buffer.append(output_buffer[-offset])
+
+                    p.update(length*8)
+
+                else:
+                    output_buffer.append(data[0:8].tobytes())
+                    del data[0:8]
+                    p.update(8)
+
+        output = b''.join(output_buffer[:-1])
+
+        with open(decompressed_file_path, "wb") as out:
+            out.write(output)
+
+        return output
+            
+
+    def _longest_match(self, text: str, current_index: int) -> Tuple[int, int]:
+        
+        lens = np.array([self._longest_match_from_index(text, i, current_index) for i in range(max(0, current_index - self.window_size), current_index)])
+        from_index = np.argmax(lens) + max(0, current_index - self.window_size) if len(lens) > 0 else None
+        length = np.max(lens) if len(lens) > 0 else 0
+
+        if length == 0:
+            return current_index, 1
+
+        return from_index, length
 
 
-def longest_match_from_index(text: str, from_index: int, current_index: int) -> int:
-    return longest_common_prefix(from_index, current_index, text)
+    def _longest_match_from_index(self, text: str, from_index: int, current_index: int) -> int:
+        return min(LZ77.longest_common_prefix(from_index, current_index, text), self.forward_size)
 
 
-@lru_cache(maxsize=1)
-def longest_common_prefix(i: int, j: int, x: str) -> int:
-    if 0 <= i < j and 0 <= j < len(x) and x[i] == x[j]:
-        return 1 + longest_common_prefix(i + 1, j + 1, x)
-    else:
-        return 0
+    @staticmethod
+    @lru_cache(maxsize=1)
+    def longest_common_prefix(i: int, j: int, x: str) -> int:
+        if 0 <= i < j and 0 <= j < len(x) and x[i] == x[j]:
+            return 1 + LZ77.longest_common_prefix(i + 1, j + 1, x)
+        else:
+            return 0
 
 
-txt = open("dickens.txt", "r").read(10000)
-# open("dickens_10k.txt", "w").write(txt)
+lz77 = LZ77(window_size=1000)
+# open("dickens_100k.txt", "w").write(txt)
 start = time.time()
-comp = compress(txt)
-print(comp)
-open("dickens_compressed_10k.txt", "wb").write(comp)
+comp = lz77.compress("dickens.txt", "dickens_compressed_1m")
+print(time.time() - start)
 
-decomp = decompress(comp)
-print(decomp)
-
-open("dickens_decompressed_10k.txt", "w").write(decomp)
+decomp = lz77.decompress("dickens_compressed_1m", "dickens_decompressed_1m.txt")
