118468c244baa25e42078407e959d12058cbc369
Ilan Prais <ilan.prais@pagaya.com>
2022-09-20 18:29:45 +0300

    updates

--- borrows_wheeler_transform.py
+++ borrows_wheeler_transform.py
@@ -1,28 +1,42 @@
-import argparse
 from operator import itemgetter
 
 class BWT():
 
-    def transform(self, txt: str):
-        n = len(txt)
-        m = sorted([txt[i:n]+txt[0:i] for i in range(n)])
-        I = m.index(txt)
-        L = ''.join([q[-1] for q in m])
-        return (I, L)
+    def suffix_array(self, arr):
+        arr_size = len(arr)
+        arr_int = {v: k for k, v in enumerate(sorted(set(arr)))}
+        arr = [arr_int[x] for x in arr]
+        arr.append(-1)
+        suf = [[i, arr[i], arr[i + 1]] for i in range(arr_size)]
+        suf.sort(key=itemgetter(1, 2))
+        idx = [0] * arr_size
+        k = 2
+        while k < arr_size:
+            r = 0
+            prev_r = suf[0][1]
+            for i in range(arr_size):
+                if suf[i][1] != prev_r or suf[i - 1][2] != suf[i][2]:
+                    r += 1
+                prev_r = suf[i][1]
+                suf[i][1] = r
+                idx[suf[i][0]] = i
+            for i in range(arr_size):
+                next_idx = suf[i][0] + k
+                suf[i][2] = suf[idx[next_idx]][1] if next_idx < arr_size else -1
+            suf.sort(key=itemgetter(1, 2))
+            k <<= 1
+        return [x[0] for x in suf]
 
-    def restore(self, I, L):
-        n = len(L)
-        X = sorted([(i, x) for i, x in enumerate(L)], key=itemgetter(1))
 
-        T = [None for i in range(n)]
-        for i, y in enumerate(X):
-            j, _ = y
-            T[j] = i
+    def transform(self, data):
+        data_ref = self.suffix_array(data)
+        return "".join(data[x-1] for x in data_ref), data_ref.index(0)
 
-        Tx = [I]
-        for i in range(1, n):
-            Tx.append(T[Tx[i-1]])
 
-        S = [L[i] for i in Tx]
-        S.reverse()
-        return ''.join(S)
+    def restore(self, data, idx):
+        out = ""
+        sorted_data_ref = [i for i, _ in sorted(enumerate(data), key=itemgetter(1))]
+        for i in range(len(data)):
+            idx = sorted_data_ref[idx]
+            out += data[idx]
+        return out
--- run_length.py
+++ run_length.py
@@ -2,6 +2,17 @@ from bitarray import bitarray
 from borrows_wheeler_transform import BWT
 
 class RLE():
+    
+    '''
+    Run-Length Encoding with BWT. 
+    Example:
+    abababab -> (BWT) -> aaaabbbb -> (RLE) -> a4b4 -> [a][0100][b][0100]
+    8 bytes -> 3 bytes
+    
+    Encoding structure:
+    [BWT - L][REP SIZE][c][rep][c][rep][c][rep]...
+        4        1      1   d   1   d   1   d    ------> 5 + length(1 + REP_SIZE) bytes
+    '''
 
     def __init__(self, bwt: BWT = None) -> None:
         self.bwt = bwt
@@ -11,11 +22,15 @@ class RLE():
         output_buffer = bitarray(endian='big')
 
         if self.bwt:
-            I, L = self.bwt.transform(open(file_path, "r").read())
+            print("Transforming...")
+            L, I = self.bwt.transform(open(file_path, "r").read())
+            print("Done transforming!")
             data = L.encode()
             output_buffer.frombytes(I.to_bytes(length=4, byteorder="big")) # L Can be up to 4 bytes in size
         else:
             data = open(file_path, "rb").read()
+            
+        print("Encoding...")
 
         i = 0
         seq_char = None
@@ -24,10 +39,10 @@ class RLE():
             if seq_len == 0:
                 seq_char = c
                 seq_len = 1
-            elif c != seq_char or seq_len >= 255: # make sure len fits in 8 bits
+            elif c != seq_char or seq_len >= 15: # make sure len fits in 4 bits
                 print(seq_char, seq_len)
                 output_buffer.frombytes(bytes([seq_char]))
-                output_buffer.frombytes(bytes([seq_len]))
+                output_buffer.extend('{0:04b}'.format(seq_len)) # 4 bits
                 seq_char = c
                 seq_len = 1
             else:
@@ -35,9 +50,11 @@ class RLE():
 
         # print(seq_char, seq_len)
         output_buffer.frombytes(bytes([seq_char]))
-        output_buffer.frombytes(bytes([seq_len]))
+        output_buffer.extend('{0:04b}'.format(seq_len)) # 4 bits
 
         output_buffer.fill()
+        
+        print("Done Encoding!")
 
         with open(compressed_file_path, "wb") as out:
             out.write(output_buffer.tobytes())
@@ -53,16 +70,24 @@ class RLE():
         if self.bwt:
             I = int.from_bytes(data[0:32], byteorder="big")
             del data[0:32]
+            
+        print("Decoding...")
 
         i = 0
-        while i <= len(data) - 16:
-            output_buffer.append(data[i:i+8].tobytes()*ord(data[i+8:i+16].tobytes()))
-            i += 16
+        while i <= len(data) - 12:
+            output_buffer.append(data[i:i+8].tobytes()*int(data[i+8:i+12].to01(), 2))
+            i += 12
 
         output = b''.join(output_buffer)
+        
+        print("Done Decoding!")
+        
+        print("Restoring...")
 
         if self.bwt:
-            output = self.bwt.restore(I, output.decode()).encode()
+            output = self.bwt.restore(output.decode(), I).encode()
+        
+        print("Done Restoring!")
 
         with open(decompressed_file_path, "wb") as out:
             out.write(output)
@@ -71,6 +96,6 @@ class RLE():
 
 rle = RLE(bwt=BWT())
 
-comp = rle.compress("dickens_200k.txt", "d_c_rl_200k")
+comp = rle.compress("dickens_500k.txt", "d_c_rl_500k")
 
-decomp = rle.decompress("d_c_rl_200k", "d_c_rl_200k_decomp.txt")
+decomp = rle.decompress("d_c_rl_500k", "d_c_rl_500k_decomp.txt")
